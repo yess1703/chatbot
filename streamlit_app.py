93b12fcfbc86d6603afe623c72bc60dbe8233abd
yess1703 <74864369+yess1703@users.noreply.github.com>
2025-01-22 09:02:32 +0000

    On branch main Your branch is up to date with 'origin/main'.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,47 @@
 import streamlit as st
-from openai import OpenAI
+from langchain_gigachat import GigaChat
+from langchain.schema import HumanMessage, AIMessage
+import sys
+import os
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+
+st.title("💬 GigaChat AI")
+st.write("This is a simple chatbot that uses GigaChat model to generate responses. ")
+
+gigachat_api_key = st.text_input("Введите ваш ключ GigaChat API", type="password")
+if not gigachat_api_key:
+    st.info("Please add your GigaChat API key to continue.", icon="🗝️")
 else:
+    client = GigaChat(
+        credentials=gigachat_api_key,
+        model="GigaChat",
+        ca_bundle_file=os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "russian_trusted_root_ca.cer"
+        ),
+    )
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
-
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
-    # Display the existing chat messages via `st.chat_message`.
     for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+        role = "user" if isinstance(message, HumanMessage) else "assistant"
+        with st.chat_message(role):
+            st.markdown(message.content)
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
+    if prompt := st.chat_input("Введите запрос"):
+        st.session_state.messages.append(HumanMessage(content=prompt))
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
+        try:
+            response = client.invoke(st.session_state.messages)
+            assistant_message = response.content
+        except Exception as e:
+            assistant_message = f"Error: {e}"
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
         with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            st.markdown(assistant_message)
+
+        st.session_state.messages.append(AIMessage(content=assistant_message))
